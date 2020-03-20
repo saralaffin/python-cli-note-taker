a7cf010f77f8a292f4f05122b54d31499c31cae2
Sara Laffin <sarala@umich.edu>
2020-03-19 22:36:24 -0400

    connect to db from notetaker and add create and read functionality

--- notetaker.py
+++ notetaker.py
@@ -1,9 +1,22 @@
 import argparse
+from peewee import *
+from datetime import date
+import time
+
+db = PostgresqlDatabase('notes',user='sara',password='sara',host='localhost',port=5432)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+class Note(BaseModel):
+    title= CharField()
+    date= DateField()
+    content= CharField()
+
+db.connect()
 
 parser = argparse.ArgumentParser(description="add notes to PostgreSQL database")
-# group = parser.add_mutually_exclusive_group()
-# group.add_argument("-v", "--verbose", action="store_true")
-# group.add_argument("-q", "--quiet", action="store_true")
+parser.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("action", help="what action you'd like to take", choices=['list','create'],default='list',)
 args = parser.parse_args()
 
@@ -11,9 +24,19 @@ user_action = args.action
 
 if user_action=='list':
     print("now showing: your notes!")
+    note_list = Note.select()
+    if args.quiet:
+        print('the short list')
+        for single_note in list(note_list):
+            print(single_note.title," ",single_note.date)
+    else:
+        print('the long list')
+        for single_note in list(note_list):
+            print(single_note.title," ",single_note.date)
 elif user_action=='create':
     title = input('Please provide the title of your new note: ')
     content = input('Please provide the content: ')
+    Note(title=title,date=time.ctime(),content=content).save()
     print(f"""Note saved! 
 Title: {title}
 Content: {content}    
